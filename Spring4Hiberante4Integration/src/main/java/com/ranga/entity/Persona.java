2847f06a34c5d741620a97bef0ad1ce4d661142f
Desarrollo <Desarrollo@Desarrollo-PC>
2017-10-06 17:23:36 -0500

    Commit No 1

---
+++ Spring4Hiberante4Integration/src/main/java/com/ranga/entity/Persona.java
@@ -0,0 +1,76 @@
+package com.ranga.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Persona")
+public class Persona {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idPersona;
+
+	@Column
+	private String nombre;
+
+	@Column
+	private String apellidos;
+
+	@Column
+	private String direccion;
+
+	@Column
+	private int edad;
+
+	public long getIdPersona() {
+		return idPersona;
+	}
+
+	public void setIdPersona(long idPersona) {
+		this.idPersona = idPersona;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [idPersona=" + idPersona + ", nombre=" + nombre + ", apellidos=" + apellidos + ", direccion="
+				+ direccion + ", edad=" + edad + "]";
+	}
+
+}
